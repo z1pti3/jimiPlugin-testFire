d7df0ea7735c8a21ec6de92beb1fd0615310644a
z1p <z1pti3@outlook.com>
2021-08-22 11:02:46 +0000

    Added default event

--- models/trigger.py
+++ models/trigger.py
@@ -2,7 +2,7 @@ from core.models import trigger
 
 
 class _testFireTrigger(trigger._trigger):
-    events = list()
+    events = [1]
 
     def check(self):
         self.result["events"] = self.events
